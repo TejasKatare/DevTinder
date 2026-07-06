b0dec83c4770366ebdbd70038413905fc7f79ce3
TejasKatare <tejaskatareofficial@gmail.com>
2026-07-06 20:41:17 +0530

    for test

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors(
         credentials: true,
     }
 ));
-
+console.log("APP STARTED");
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', testRouter);
--- src/routes/test.js
+++ src/routes/test.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const testRouter = express.Router();
 
+console.log("TEST ROUTER LOADED");
 testRouter.get('/', (req, res) => {
+    console.log("ROOT ROUTE HIT");
     res.send('Welcome Please Login or SignUp');
 });
 
